37c31fe8aa32baface1111227c0304c2a649f5f3
damlaimre9 <imredamla9@gmail.com>
2024-12-20 19:17:55 +0300

    some changes
    sql raw queries added to login regirster functions

--- database_project/authentication/views.py
+++ database_project/authentication/views.py
@@ -1,26 +1,8 @@
 
-from django.db import connection
-
-#class LoginView(View):
- #   template_name = 'authentication/login.html' 
-  #  def get(self, request, *args, **kwargs):
-   #     return render(request, self.template_name)   
-
-#class RegisterView(View):
- #   template_name = 'authentication/register.html' 
-  #  def get(self, request, *args, **kwargs):
-   #     return render(request, self.template_name)   
-
-#class ChangePasswordView(View):
- #   template_name = 'authentication/change_password.html' 
-  #  def get(self, request, *args, **kwargs):
-   #     return render(request, self.template_name)  
-    
 from django.shortcuts import render, redirect
 from django.views import View
-from django.db.models import Q
+from django.db import connection
 from django.urls import reverse
-from .models import User
 
 class LoginView(View):
     template_name = 'authentication/login.html'
@@ -32,17 +14,16 @@ class LoginView(View):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        try:
-            # Check if user exists
-            user = User.objects.get(username=username, password=password)
-            # Set session
-            request.session['user_id'] = user.user_id
-            return redirect('main')
-        except User.DoesNotExist:
-            # User not found or wrong password
-            return render(request, self.template_name, {
-                'error': 'Invalid username or password'
-            })
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT user_id FROM users WHERE username=%s AND password=%s", [username, password])
+            user = cursor.fetchone()
+
+        if user:
+            # user is a tuple like (user_id,)
+            request.session['user_id'] = user[0]
+            return redirect('my_courses')  # after login go to my_courses page
+        else:
+            return render(request, self.template_name, {'error': 'Invalid username or password'})
 
 
 class RegisterView(View):
@@ -56,25 +37,26 @@ class RegisterView(View):
         name = request.POST.get('name')
         surname = request.POST.get('surname')
         password = request.POST.get('password')
-        user_type = request.POST.get('user-type')  # If provided
+        user_type = request.POST.get('user-type')  # you may or may not use this
 
-        # Check if user already exists
-        if User.objects.filter(username=username).exists():
-            return render(request, self.template_name, {
-                'error': 'Username already taken'
-            })
+        # Check if username exists
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT user_id FROM users WHERE username=%s", [username])
+            existing = cursor.fetchone()
 
-        # Create new user
-        user = User.objects.create(
-            username=username,
-            forename=name,
-            surname=surname,
-            password=password,
-            account_money=0.00,
-            # user_type=user_type if you have this field
-        )
+        if existing:
+            return render(request, self.template_name, {'error': 'Username already taken'})
 
-        # After registration, user can be redirected to login page
+        # Insert new user
+        # Assuming user_id is auto-increment in db:
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO users (forename, surname, username, password, account_money)
+                VALUES (%s, %s, %s, %s, 0)
+                RETURNING user_id
+            """, [name, surname, username, password])
+            new_user_id = cursor.fetchone()[0]
+
         return redirect('login')
 
 
@@ -83,31 +65,28 @@ class ChangePasswordView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
     def post(self, request, *args, **kwargs):
         username = request.POST.get('username')
         old_password = request.POST.get('password')
         new_password = request.POST.get('new-password')
 
-        try:
-            user = User.objects.get(username=username, password=old_password)
-            user.password = new_password
-            user.save()
-            return redirect('login')
-        except User.DoesNotExist:
-            return render(request, self.template_name, {
-                'error': 'Invalid username or old password'
-            })
+        with connection.cursor() as cursor:
+            # Check if user exists with old password
+            cursor.execute("SELECT user_id FROM users WHERE username=%s AND password=%s", [username, old_password])
+            user = cursor.fetchone()
 
-class MainView(View):
-    template_name = 'authentication/main.html'
-
-    def get(self, request, *args, **kwargs):
-        # Check if user_id is in session, if not redirect to login
-        if 'user_id' not in request.session:
+        if user:
+            # Update password
+            with connection.cursor() as cursor:
+                cursor.execute("UPDATE users SET password=%s WHERE username=%s", [new_password, username])
             return redirect('login')
-        
-        user_id = request.session['user_id']
-        user = User.objects.get(pk=user_id)
-        return render(request, self.template_name, {'user': user})
+        else:
+            return render(request, self.template_name, {'error': 'Invalid username or old password'})
+
+
+class LogoutView(View):
+    def get(self, request):
+        request.session.flush()  # Clear session
+        return redirect('login')
 
